4cc295a6cb450217e99015dda3cab2ecc6e83749
jojo <joannek715967@gmail.com>
2023-02-16 09:59:13 -0800

    I finished my project as well as the plane programming
    I fixed the errors of crates, i added the rigidbody. i also finished the plane programming so it tilts with the up and down arrows while the camera follows.

--- Unit1PlayerControll/Assets/Challenge 1/Scripts/PlaneControllerX.cs
+++ Unit1PlayerControll/Assets/Challenge 1/Scripts/PlaneControllerX.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerX : MonoBehaviour
+public class PlaneControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +21,14 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        
+        
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
  
